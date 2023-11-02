37e3ad83fbab4c96f8ae71a92b62f820706cf269
Armaan Gohil <46198340+Armaan1Gohil@users.noreply.github.com>
2023-11-02 20:30:12 +0530

    Added Microsoft Clarity
    Added web analytical tool

--- app.py
+++ app.py
@@ -163,7 +163,15 @@ server = app.server
 app.title = 'Train Schedule' 
  
 app.layout = dbc.Container(
-    [   
+    [   html.Script(
+            '''
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "jhgwrliuco");
+            '''
+        ),
         html.Br(),
         dbc.Card(
             [
